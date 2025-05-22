d3049a81aa23f3655b1b620dc2a434968748f343
Patryk Lewandowski <patryklew99@wp.pl>
2025-05-23 01:21:12 +0200

    Menu upgrade
    Menu was enhanced and is almost done

--- views/main_window.py
+++ views/main_window.py
@@ -1,11 +1,11 @@
 import os
 
 import PySide6
-from PySide6.QtCore import QPropertyAnimation
 from PySide6.QtWidgets import QMainWindow, QLabel, QPushButton, QWidget, QVBoxLayout, QFileDialog, QHBoxLayout, \
     QComboBox, QFormLayout, QLineEdit, QScrollArea, QToolButton, QSizePolicy, QFrame
 
-from PySide6.QtCore import Qt, QPropertyAnimation, QSize
+from PySide6.QtCore import Qt
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -38,10 +38,20 @@ class MainWindow(QMainWindow):
         self.iv_edit = QLineEdit()
 
         self.encrypt_btn = QPushButton("Encrypt")
-        self.decrypt_btn = QPushButton("Decrypt")
-        self.gen_key_and_iv = QPushButton("Generate Key and IV")
+        self.encrypt_btn.setMinimumHeight(32)
+        self.encrypt_btn.setObjectName("config_btn")
+        self.encrypt_btn.setCursor(Qt.CursorShape.PointingHandCursor)
 
-        self.success_txt = QLabel("Operacja zakończona sukcesem!")
+        self.decrypt_btn = QPushButton("Decrypt")
+        self.decrypt_btn.setMinimumHeight(32)
+        self.decrypt_btn.setObjectName("config_btn")
+
+        self.gen_key_and_iv = QPushButton("Generate Key and IV")
+        self.gen_key_and_iv.setMinimumHeight(32)
+        self.gen_key_and_iv.setObjectName("config_btn")
+
+        self.info_label = QLabel("Operacja zakończona sukcesem!")
+        self.info_label.setObjectName("info_label")
 
         config_container = QWidget()
         config_container.setObjectName("configuration_container")
@@ -55,6 +65,8 @@ class MainWindow(QMainWindow):
         file_layout = QVBoxLayout()
         file_layout.addWidget(self.file_label)
         file_layout.addWidget(self.file_btn)
+
+        file_layout.setContentsMargins(0, 0, 0, 40)
 
         config_layout.addLayout(file_layout)
 
@@ -71,19 +83,13 @@ class MainWindow(QMainWindow):
         form.addRow(self.decrypt_btn)
         form.addRow(self.gen_key_and_iv)
 
-        collapsible = CollapsibleBox("Testowe ustawienia błędu")
-        collapsible.add_widget(QLabel("djfnskd"))
-
-
+        form.setContentsMargins(0, 0, 0, 20)
         config_layout.addLayout(form)
-        config_layout.addWidget(self.success_txt)
-        config_layout.addWidget(collapsible)
+        config_layout.addWidget(self.info_label)
+
         # 3) Główny układ
         main_layout = QHBoxLayout(central)
-        main_layout.addLayout(file_layout)
         main_layout.addWidget(config_container)
-
-        #self.setStyleSheet(load_stylesheet())
 
 
     def choose_file(self):
@@ -97,51 +103,3 @@ class MainWindow(QMainWindow):
         if file_path:
             self.select_file_label.setText(f"Wybrano: {os.path.basename(file_path)}")
             self.process_file(file_path) #TODO
-
-
-class CollapsibleBox(QWidget):
-    def __init__(self, title="", parent=None):
-        super().__init__(parent)
-
-        self.toggle_button = QToolButton(text=title, checkable=True, checked=False)
-        self.toggle_button.setStyleSheet("QToolButton { font-weight: bold; }")
-        self.toggle_button.setToolButtonStyle(Qt.ToolButtonTextBesideIcon)
-        self.toggle_button.setArrowType(Qt.RightArrow)
-        self.toggle_button.clicked.connect(self.on_toggle)
-
-        self.content_area = QScrollArea(maximumHeight=0, minimumHeight=0)
-        self.content_area.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
-        self.content_area.setFrameShape(QFrame.Shape.NoFrame)
-        self.content_area.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
-        self.content_area.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
-
-        self.toggle_animation = QPropertyAnimation(self.content_area, b"maximumHeight")
-        self.toggle_animation.setDuration(200)
-        self.toggle_animation.setStartValue(0)
-        self.toggle_animation.setEndValue(0)
-
-        main_layout = QVBoxLayout(self)
-        main_layout.addWidget(self.toggle_button)
-        main_layout.addWidget(self.content_area)
-        main_layout.setContentsMargins(0, 0, 0, 0)
-
-        self.content_layout = QVBoxLayout()
-        self.content_layout.setAlignment(Qt.AlignTop)
-        self.content_widget = QWidget()
-        self.content_widget.setLayout(self.content_layout)
-        self.content_area.setWidget(self.content_widget)
-        self.content_area.setWidgetResizable(True)
-
-    def on_toggle(self):
-        checked = self.toggle_button.isChecked()
-        self.toggle_button.setArrowType(Qt.ArrowType.DownArrow if checked else Qt.ArrowType.RightArrow)
-
-        total_height = self.content_widget.sizeHint().height()
-        self.toggle_animation.setDirection(
-            QPropertyAnimation.Direction.Forward if checked else QPropertyAnimation.Direction.Backward
-        )
-        self.toggle_animation.setEndValue(total_height)
-        self.toggle_animation.start()
-
-    def add_widget(self, widget):
-        self.content_layout.addWidget(widget)
